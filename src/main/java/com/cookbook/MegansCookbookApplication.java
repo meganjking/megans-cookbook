e4e6b3a8b39221789528b880dc541032079875f3
Francois Mariette <karrde00@gmail.com>
2016-06-20 19:52:12 -0500

    first commit

---
+++ src/main/java/com/cookbook/MegansCookbookApplication.java
@@ -0,0 +1,12 @@
+package com.cookbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MegansCookbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MegansCookbookApplication.class, args);
+	}
+}
